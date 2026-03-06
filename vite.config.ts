4f0b01b7a77be89243fc02c45e4a3ef463521429
Utkarsh Purbey <uttu_codes@Utkarshs-MacBook-Air.local>
2026-03-07 00:18:42 +0530

    Demo

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(() => ({
-  plugins: [react(), tailwindcss()],
-  // GitHub Pages serves at https://<user>.github.io/<repo-name>/ so we need base in CI
-  base: process.env.BASE_PATH ?? '/',
-}))
+export default defineConfig({
+  plugins: [react(), tailwindcss(),],
+})
